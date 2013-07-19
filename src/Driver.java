d2a1dafdaf13be0e28ee96341bd668c453e32c15
Mondroid <richmond_liclican@dlsu.ph>
2013-07-19 10:07:14 +0800

    This is still Dog. Modified Driver. Added display method.

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,16 @@
 public class Driver {
 
 	public static void main(String[] args) {
+		new Driver();
+	}
+	
+	public Driver(){
 		System.out.println("Hello World!");
+		
+		Display("This is Dog.");
 	}
 
+	public void Display(String sentence){
+		System.out.println(sentence);
+	}
 }
